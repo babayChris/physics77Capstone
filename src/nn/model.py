005e0b940a88e2bbaef3c3a9d975f76063bc246c
Shagun Juthani <shagun_juthani@berkeley.edu>
2025-07-28 16:55:24 -0700

    create model.py

---
+++ src/nn/model.py
@@ -0,0 +1,9 @@
+"""Important step to modify the optimizer and tailor it to our model"""
+def model():
+   """
+   Simple Linear Regression: f(x) = m * x + b?
+   Exponential = a * np.exp(b * x)
+   etc...
+   """
+  
+   return 
