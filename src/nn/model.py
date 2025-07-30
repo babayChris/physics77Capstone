f9ed32e88861e3ca5a3538a0fefc4b193d8d1100
mrNobody <100326160+unknownHumanoid-m@users.noreply.github.com>
2025-07-30 14:57:30 -0700

    Delete src/nn/model.py

--- src/nn/model.py
+++
@@ -1,9 +0,0 @@
-"""Important step to modify the optimizer and tailor it to our model"""
-def model():
-   """
-   Simple Linear Regression: f(x) = m * x + b?
-   Exponential = a * np.exp(b * x)
-   etc...
-   """
-  
-   return 
