435d5c126dc4e298d472da554cccc224f822df22
mrNobody <100326160+unknownHumanoid-m@users.noreply.github.com>
2025-08-05 08:19:50 -0700

    Update loss.py
    fixed it to include forward softmax pass

--- src/nn/loss.py
+++ src/nn/loss.py
@@ -13,12 +13,11 @@ class Loss:
     def loss(self, pred: np.ndarray, truth: np.ndarray) -> float:
         if truth.ndim > 1:
             truth = truth.flatten()
-        
-        pred_clipped = np.clip(pred, 1e-15, 1 - 1e-15)
-        
-        probs = pred_clipped[np.arange(len(truth)), truth.astype(int)]
-        
-        loss = -np.mean(np.log(probs))
+
+        logSoft = self.logSoftmax(pred) 
+    
+        probs = logSoft[np.arange(len(truth)), truth.astype(int)]
+        loss = -np.mean(probs)
         
         return loss
 
@@ -27,10 +26,19 @@ class Loss:
         """
         calculates gradient of loss for sparse categorical cross entropy 
         """
+        sofmaxPred = np.exp(self.logSoftmax(pred))
         if truth.ndim > 1:
             truth = truth.flatten()
             
         truth_one_hot = np.zeros_like(pred)
+        truth_one_hot[np.arange(len(truth)), truth.astype(int)] = 1
+        
+        return (sofmaxPred - truth_one_hot) / pred.shape[0]
+
+
+    def logSoftmax(self, pred: np.ndarray) -> np.ndarray:
+        shifted_inputs = pred - np.max(pred, axis=-1, keepdims=True)
+        return shifted_inputs - np.log(np.sum(np.exp(shifted_inputs), axis=1, keepdims=True))
         truth_one_hot[np.arange(len(truth)), truth.astype(int)] = 1
         
         return (pred - truth_one_hot) / pred.shape[0]
